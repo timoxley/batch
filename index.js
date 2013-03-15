a5cea752523b488fed4451d5a785b983d68e0e45
Tim Oxley <secoif@gmail.com>
2013-03-15 13:08:29 +0800

    EventEmitter only exists in Node

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports = Batch;
  */
 
 function Batch() {
-  EventEmitter.call(this);
+  if (EventEmitter) EventEmitter.call(this);
   this.fns = [];
   this.concurrency(Infinity);
   for (var i = 0, len = arguments.length; i < len; ++i) {
